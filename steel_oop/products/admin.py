7c46fedc8a4c38bc9f8d300eccd28c994ab490ab
Владимир Шалдыкин <shaldykin@gmail.com>
2023-03-06 17:23:08 +0300

    filters in admin

--- steel_oop/products/admin.py
+++ steel_oop/products/admin.py
@@ -1,60 +1,75 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 
-# from . models import Category, Product, ProductSpecificationValue, Subcategory, Specification
-
-
-# class SpecificationValueInline(admin.TabularInline):
-#     model = ProductSpecificationValue
-#     min_num = 1
-#     extra = 0
+from . models import Category, ProductList, ProductNotList, Subcategory
 
 
-# @admin.register(Category)
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ('name_cat',)
-#     search_fields = ('name_cat',)
-#     # list_editable = ('name_cat',)
-#     empty_value_display = '-пусто-'
+class CategoryListFilter(admin.SimpleListFilter):
+    title = ('Группы товаров')
+    parameter_name = 'category'
+    
+    def lookups(self, request, model_admin):
+        (
+            ('listovoy', ('листовой')),
+            ('90s', ('sortovoy')),
+            ('prokat_trub', ('прокат труб')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'listovoy':
+            return queryset.filter(
+               
+            )
+        if self.value() == '90s':
+            return queryset.filter(
+                birthday__gte=date(1990, 1, 1),
+                birthday__lte=date(1999, 12, 31),
+            )    
 
 
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ('subcategory', 'show_specifications', 'base_price', 'discount',)
-#     search_fields = ('subcategory',)
-#     list_filter = ('subcategory',)
-#     # list_editable = ('base_price', 'discount',)
-#     empty_value_display = '-пусто-'
-#     inlines = (
-#         SpecificationValueInline,
-#     )
-
-#     def show_specifications(self, obj):
-#         specification_list = []
-#         for specification in obj.specifications.all():
-#             specification_list.append(specification.name)
-#         return specification_list
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name_cat', 'slug',)
+    search_fields = ('name_cat',)
+    # list_editable = ('name_cat',)
 
 
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'slug',)
+    search_fields = ('name',)
+    list_filter = ('category',)
+    list_editable = ('slug',)
+    
+    
 
-# @admin.register(ProductSpecificationValue)
-# class ProductSpecificationValueAdmin(admin.ModelAdmin):
-#     list_display = ('product', 'specification', 'value',)
-#     search_fields = ('product',)
-#     # list_editable = ('value',)
-#     empty_value_display = '-пусто-'
+@admin.register(ProductNotList)
+class ProductNotListAdmin(admin.ModelAdmin):
+    list_display = ('size', 'parameter', 'length', 'show_price_tonn',
+                    'show_price_metr', 'coeff', 'base_price', 'discount',
+                    )
+    search_fields = ('size',)
+    list_filter = ('subcategory',)
+    list_editable = ('base_price', 'discount',)
+
+    def show_price_tonn(self, obj):
+        return obj.price_tonn
+
+    def show_price_metr(self, obj):
+        return obj.price_metr
 
 
-# @admin.register(Subcategory)
-# class SubcategoryAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'category',)
-#     search_fields = ('name',)
-#     # list_editable = ('slug',)
-#     empty_value_display = '-пусто-'
+@admin.register(ProductList)
+class ProductListAdmin(admin.ModelAdmin):
+    list_display = ('thickness', 'size', 'area', 'show_price_tonn',
+                    'show_price_item', 'coeff', 'base_price', 'discount',
+                    )
+    search_fields = ('thickness',)
+    list_filter = ('subcategory',)
+    list_editable = ('base_price', 'discount',)
 
+    def show_price_tonn(self, obj):
+        return obj.price_tonn
 
-# @admin.register(Specification)
-# class SpecificationAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'subcategory',)
-#     search_fields = ('name',)
-#     # list_editable = ('name',)
-#     empty_value_display = '-пусто-'
+    def show_price_item(self, obj):
+        return obj.price_item
--- steel_oop/products/models.py
+++ steel_oop/products/models.py
@@ -1,118 +1,131 @@
-# from django.db import models
-# from django.contrib.auth import get_user_model
-# from django.shortcuts import reverse
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
+from django.shortcuts import reverse
 
 
-# User = get_user_model()
+User = get_user_model()
 
 
-# class Category(models.Model):
-#     name_cat = models.CharField('Группа товаров', max_length=250)
-#     slug = models.SlugField(max_length=50, unique=True)
+class Category(models.Model):
+    name_cat = models.CharField('Группа товаров', max_length=250)
+    slug = models.SlugField(max_length=50, unique=True)
 
-#     def get_absolute_url(self):
-#         return reverse('products:products_category',  args=[self.slug])
-    
-#     def __str__(self):
-#         return self.name_cat
-    
-#     class Meta:
-#         verbose_name = 'Группа товаров'
-#         verbose_name_plural = 'Группы товаров'
+    def get_absolute_url(self):
+        return reverse('products:products_category', args=[self.slug])
 
+    def __str__(self):
+        return self.name_cat
 
-# class Subcategory(models.Model):
-#     name = models.CharField('Категория товаров', max_length=250)
-#     slug = models.SlugField(max_length=50, unique=True)
-#     category = models.ForeignKey(
-#         Category, 
-#         verbose_name='Группа товаров', 
-#         on_delete=models.CASCADE,
-#         related_name='subcategories'
-#         )
-
-#     def get_absolute_url(self):
-#         return reverse('products:products_subcategory',
-#                        kwargs={'cat_slug': self.category.slug,
-#                                'subcat_slug': self.slug}
-#                        )
-
-#     def __str__(self):
-#         return self.name
-    
-#     class Meta:
-#         verbose_name = 'Категория товаров'
-#         verbose_name_plural = 'Категории товаров'
+    class Meta:
+        verbose_name = 'Группа товаров'
+        verbose_name_plural = 'Группы товаров'
 
 
-# class Specification(models.Model):
-#     name = models.CharField('Характеристика', max_length=250)
-#     subcategory = models.ForeignKey(
-#         Subcategory, 
-#         verbose_name='Категория товаров', 
-#         on_delete=models.CASCADE,
-#         related_name='specifications'
-#         )
-    
-#     class Meta:
-#         verbose_name = 'Характеристика товара'
-#         verbose_name_plural = 'Характеристики товаров'
+class Subcategory(models.Model):
+    name = models.CharField('Категория товаров', max_length=250)
+    slug = models.SlugField(max_length=50, unique=True)
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Группа товаров',
+        on_delete=models.CASCADE,
+        related_name='subcategories'
+    )
 
-#     def __str__(self):
-#         return self.name    
+    def get_absolute_url(self):
+        return reverse('products:products_subcategory',
+                       kwargs={'cat_slug': self.category.slug,
+                               'subcat_slug': self.slug}
+                       )
 
+    def __str__(self):
+        return self.name
 
-# class Product(models.Model):
-#     subcategory = models.ForeignKey(
-#         Subcategory,
-#         on_delete=models.CASCADE,
-#         related_name='products',
-#         verbose_name='Категория товара',
-#         )
-#     base_price = models.IntegerField('Базовая цена')
-#     coeff_tonne_metre = models.IntegerField('Коэфф. пересчета тонны в метры')
-#     discount = models.IntegerField('Скидка в процентах', blank=True, default=0)
-    
-#     specifications = models.ManyToManyField(
-#         'Specification',        
-#         through='ProductSpecificationValue',
-#         related_name='products',
-#         verbose_name='характеристики'
-#     )
-
-#     @property
-#     def get_discount(self):
-#         '''Расчитать стоимость со скидкой'''
-#         price = int(self.price * (100 - self.discount) / 100)
-#         return price
-
-#     def __str__(self):
-#         return self.subcategory.name
-
-#     class Meta:
-#         ordering = ['-base_price']
-#         verbose_name = 'Товар'
-#         verbose_name_plural = 'Товары'
+    class Meta:
+        verbose_name = 'Категория товаров'
+        verbose_name_plural = 'Категории товаров'
 
 
-# class ProductSpecificationValue(models.Model):
-#     product = models.ForeignKey(
-#         Product,
-#         on_delete=models.CASCADE,
-#         related_name='value'
-#         )
-#     specification = models.ForeignKey(
-#         Specification,
-#         on_delete=models.CASCADE,
-#         related_name='value'
-#         )
-#     value = models.CharField('Значение', max_length=50)
+class ProductNotList(models.Model):
+    size = models.CharField('Размер', max_length=50)
+    parameter = models.CharField('Параметры, марка стали', max_length=50)
+    length = models.IntegerField('Длина')
+    subcategory = models.ForeignKey(
+        Subcategory,
+        on_delete=models.CASCADE,
+        related_name='products_not_list',
+        verbose_name='Категория товара',
+    )
+    base_price = models.IntegerField('Базовая цена')
+    coeff = models.DecimalField(
+        'Коэфф. пересчета',
+        max_digits=7,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(1)])
+    discount = models.IntegerField('Скидка в процентах', blank=True, default=0)
 
-#     def __str__(self):
-#         return self.value
-    
-#     class Meta:
-#         verbose_name = 'Значение характеристики'
-#         verbose_name_plural = 'Значение характеристик'
+    @property
+    def price_tonn(self):
+        '''Расчитать стоимость со скидкой'''
+        price_tonn = int(self.price * (100 - self.discount) / 100)
+        return price_tonn
 
-            
+    @property
+    def price_metr(self):
+        "Расчитать стоимость тонны"
+        price_metr = self.discount_price / self.coeff
+        return round(price_metr, 1)
+
+    def __str__(self):
+        return self.subcategory.name
+
+    class Meta:
+        ordering = ['-base_price']
+        verbose_name = 'Товар не лист'
+        verbose_name_plural = ' Товары не листы'
+
+
+class ProductList(models.Model):
+    thickness = models.CharField('Толщина', max_length=50)
+    size = models.CharField('Размер', max_length=50)
+    area = models.DecimalField(
+        'Квадратные метры',
+        max_digits=7,
+        decimal_places=3,
+        validators=[
+            MinValueValidator(1)])
+    subcategory = models.ForeignKey(
+        Subcategory,
+        on_delete=models.CASCADE,
+        related_name='products_list',
+        verbose_name='Категория товара',
+    )
+    base_price = models.IntegerField('Базовая цена')
+    coeff = models.DecimalField(
+        'Коэфф. пересчета',
+        max_digits=7,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(1)])
+    discount = models.IntegerField('Скидка в процентах', blank=True, default=0)
+
+    @property
+    def price_tonn(self):
+        '''Расчитать стоимость со скидкой'''
+        price_discount = int(self.price * (100 - self.discount) / 100)
+        return price_discount
+
+    @property
+    def price_item(self):
+        "Расчитать стоимость тонны"
+        price_metr = self.discount_price / self.coeff
+        return round(price_metr, 1)
+
+    def __str__(self):
+        return self.subcategory.name
+
+    class Meta:
+        ordering = ['-base_price']
+        verbose_name = 'Лист'
+        verbose_name_plural = ' Листы'
